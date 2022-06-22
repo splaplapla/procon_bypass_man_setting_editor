8aa38ccf5d97eaf998f3b86011df315c8938375e
jiikko <n905i.1214@gmail.com>
2022-06-22 17:55:36 +0900

    wrap

---
+++ src/components/editor.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Editor: React.FC = () => {
+  return(
+    <>
+      <h1>設定ファイルエディタ</h1>
+      <div>
+        <a href="#" >
+          エクスポートする
+        </a>
+      </div>
+
+      <h3>インストール可能なマクロ</h3>
+      xxx<br />
+      <h3>設定中のプレフィックスキー</h3>
+      xxx<br />
+    </>
+  )
+}
--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,19 +1,10 @@
 import React from "react";
+import { Editor } from '../components/editor'
 
 export const Top: React.FC = () => {
   return(
     <>
-      <h1>設定ファイルエディタ</h1>
-      <div>
-        <a href="#" >
-          エクスポートする
-        </a>
-      </div>
-
-      <h3>インストール可能なマクロ</h3>
-      xxx<br />
-      <h3>設定中のプレフィックスキー</h3>
-      xxx<br />
+      <Editor />
     </>
   )
 };
