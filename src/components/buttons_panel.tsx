6c7dc6c1e0728a9ca1a14e8c6f42c81b450d6347
jiikko <n905i.1214@gmail.com>
2022-06-25 19:43:28 +0900

    ちょっと

--- src/components/buttons_panel.tsx
+++ src/components/buttons_panel.tsx
@@ -1,15 +1,22 @@
 import { jsx, css } from "@emotion/react";
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { LayerKey } from "../types/layer_key";
+import { ButtonsSettingContext } from "./../contexts/buttons_setting";
+import { MacroSetting } from "./../components/macro_setting";
 
 type Props = {
-  layerKey: string;
+  layerKey: LayerKey;
 };
 
 export const ButtonsPanel: React.FC<Props> = ({ layerKey }) => {
+  const { setting, settingDispatch } = useContext(ButtonsSettingContext);
+
   return (
     <>
-      ここにボタンがだーって出ます({layerKey})
-      <br />
+      <h4>マクロ</h4>
+      <MacroSetting layerKey={layerKey} />
+
+      <h4>各ボタンの設定</h4>
     </>
   );
 };
---
+++ src/components/macro_setting.tsx
@@ -0,0 +1,13 @@
+import { jsx, css } from "@emotion/react";
+import React, { useState } from "react";
+import { LayerKey } from "../types/layer_key";
+
+type Props = {
+  layerKey: LayerKey;
+};
+
+export const MacroSetting: React.FC<Props> = ({ layerKey }) => {
+  return(
+    <></>
+  )
+}
