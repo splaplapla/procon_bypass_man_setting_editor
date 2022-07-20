f9de91e7c77549f88b6ee5f4a51d5fd66163849d
jiikko <n905i.1214@gmail.com>
2022-07-20 18:14:47 +0900

    correct plugin class name

--- src/types/plugin.ts
+++ src/types/plugin.ts
@@ -13,53 +13,46 @@ export type Plugin = {
 // plugins.
 export const AvailablePlugins: Array<Plugin> = [
   {
-    sumabura: {
-      modes: [],
-      macros: [
-        {
-          display_name: "a sample macro", // 後で消す
-          class_namespace: "ProconBypassMan::Sumabura::Macro::FastReturn",
-        },
-      ],
-    },
-  },
-  {
     splatoon2: {
       modes: [
         {
           display_name: "splatoon2.guruguru",
-          class_namespace: "ProconBypassMan::Splatoon2::Mode::Guruguru",
+          class_namespace: "ProconBypassMan::Plugin::Splatoon2::Mode::Guruguru",
         },
       ],
       macros: [
         {
           display_name: "試合中にリスポーンにスーパージャンプ",
-          class_namespace: "ProconBypassMan::Splatoon2::Macro::FastReturn",
+          class_namespace:
+            "ProconBypassMan::Plugin::Splatoon2::Macro::FastReturn",
         },
         {
           display_name:
             "試合中に右キーに割り当てられている味方へのスーパージャンプ",
-          class_namespace: "ProconBypassMan::Splatoon2::Macro::JumpToRightKey",
+          class_namespace:
+            "ProconBypassMan::Plugin::Splatoon2::Macro::JumpToRightKey",
         },
         {
           display_name:
             "試合中に上キーに割り当てられている味方へのスーパージャンプ",
-          class_namespace: "ProconBypassMan::Splatoon2::Macro::JumpToUpKey",
+          class_namespace:
+            "ProconBypassMan::Plugin::Splatoon2::Macro::JumpToUpKey",
         },
         {
           display_name:
             "試合中に左キーに割り当てられている味方へのスーパージャンプ",
-          class_namespace: "ProconBypassMan::Splatoon2::Macro::JumpToLeftKey",
+          class_namespace:
+            "ProconBypassMan::Plugin::Splatoon2::Macro::JumpToLeftKey",
         },
         {
           display_name: "炭酸ボムのチャージ",
           class_namespace:
-            "ProconBypassMan::Splatoon2::Macro::ChargeTansanBomb",
+            "ProconBypassMan::Splatoon2::Plugin::Macro::ChargeTansanBomb",
         },
         {
           display_name: "バブル即割",
           class_namespace:
-            "ProconBypassMan::Splatoon2::Macro::SokuwariForSplashBomb",
+            "ProconBypassMan::Splatoon2::Plugin::Macro::SokuwariForSplashBomb",
         },
       ],
     },
