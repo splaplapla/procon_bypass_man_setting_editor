bc0f3df6cd34e943faea6b34b615628dfce8eab9
jiikko <n905i.1214@gmail.com>
2022-07-20 16:52:26 +0900

    macro構文の出力に対応する

--- src/lib/setting_textualization.test.ts
+++ src/lib/setting_textualization.test.ts
@@ -1,55 +1,73 @@
+import { Button, buttons } from "../types/button";
 import { SettingTextualization } from "./setting_textualization";
+import _ from "lodash";
 import yaml from "js-yaml";
+
+function makeEmptyData() {
+  const defaultLayer = buttons.reduce((acc, item) => {
+    acc[item] = { open: false };
+    return acc;
+  }, {} as ButtonsInLayer);
+  const layers = {
+    up: _.cloneDeep(defaultLayer),
+    down: _.cloneDeep(defaultLayer),
+    left: _.cloneDeep(defaultLayer),
+    right: _.cloneDeep(defaultLayer),
+  };
+
+  return { layers }
+}
+
 
 describe("内容が空のとき", () => {
   it("validなyamlであること", () => {
-    const actual = SettingTextualization({});
+    const actual = SettingTextualization({ layers: makeEmptyData().layers });
     yaml.load(actual);
   });
 })
 
 describe("prefixKeysに値があるとき", () => {
   it("validなyamlであること", () => {
-    const actual = SettingTextualization({ prefixKeys: ["b"] });
+    const actual = SettingTextualization({ layers: makeEmptyData().layers, prefixKeys: ["b"] });
     yaml.load(actual);
   });
 
   it("設定ファイルを出力すること", () => {
-    const actual = SettingTextualization({ layers: null, prefixKeys: ["b"], installed_macros: null });
+    const actual = SettingTextualization({ layers: makeEmptyData().layers, prefixKeys: ["b"], installed_macros: null });
     const expected = `version: 1.0
 setting: |-
   prefix_keys_for_changing_layer %i(b)
-
   layer :up do
-  end
 
+  end
   layer :right do
-  end
 
+  end
   layer :down do
-  end
 
+  end
   layer :left do
+
   end`;
     expect(actual).toBe(expected);
   });
 
-  it("設定ファイルを出力すること", () => {
-    const actual = SettingTextualization({ layers: null, prefixKeys: ["b", "y"], installed_macros: null });
+  it("設定ファイルを出力すること2", () => {
+    const actual = SettingTextualization({ layers: makeEmptyData().layers, prefixKeys: ["b", "y"], installed_macros: null });
     const expected = `version: 1.0
 setting: |-
   prefix_keys_for_changing_layer %i(b y)
-
   layer :up do
-  end
 
+  end
   layer :right do
-  end
 
+  end
   layer :down do
-  end
 
+  end
   layer :left do
+
   end`;
     expect(actual).toBe(expected);
   });
@@ -57,25 +75,106 @@ setting: |-
 
 describe("installed_macrosに値があるとき", () => {
   it("設定ファイルを出力すること", () => {
-    const actual = SettingTextualization({ layers: null, prefixKeys: null, installed_macros: { AAA: true, BBB: true } });
+    const actual = SettingTextualization({ layers: makeEmptyData().layers, prefixKeys: null, installed_macros: { AAA: true, BBB: true } });
+    yaml.load(actual);
     const expected = `version: 1.0
 setting: |-
   install_macro_plugin AAA
   install_macro_plugin BBB
 
   prefix_keys_for_changing_layer %i()
-
   layer :up do
-  end
 
+  end
   layer :right do
-  end
 
+  end
   layer :down do
-  end
 
+  end
   layer :left do
+
   end`;
     expect(actual).toBe(expected);
   });
+})
+
+
+describe("layer.#{button}.flipに値があるとき", () => {
+  it("設定ファイルを出力すること", () => {
+    const layers = makeEmptyData().layers;
+    layers.up.a = {
+      flip: { if_pressed: [], enable: true, force_neutral: ["y"] },
+      open: true,
+    };
+    layers.up.b = {
+      flip: { if_pressed: ["a"], enable: true, force_neutral: ["y"] },
+      open: true,
+    };
+    layers.up.y = {
+      flip: { if_pressed: [], enable: false, force_neutral: [] },
+      open: true,
+    };
+    layers.down.y = {
+      flip: { if_pressed: ["a", "b"], enable: true, force_neutral: ["y", "x"] },
+      open: true,
+    };
+
+    const actual = SettingTextualization({ layers: layers, prefixKeys: null });
+    yaml.load(actual);
+    const expected = `version: 1.0
+setting: |-
+  prefix_keys_for_changing_layer %i()
+  layer :up do
+    flip :a, force_neutral: %i(y)
+    flip :b, if_pressed: %i(a), force_neutral: %i(y)
+
+  end
+  layer :right do
+
+  end
+  layer :down do
+    flip :y, if_pressed: %i(a b), force_neutral: %i(y x)
+
+  end
+  layer :left do
+
+  end`;
+    expect(actual).toBe(expected);
+  });
+})
+
+
+describe("layer.#{button}.macroに値があるとき", () => {
+  it("設定ファイルを出力すること", () => {
+    const layers = makeEmptyData().layers;
+    layers.up.macro = {
+      "ProconBypassMan::Splatoon2::Macro::FastReturn": ["y", "l"],
+    };
+    layers.down.y = {
+      flip: { if_pressed: ["a", "b"], enable: true, force_neutral: ["y", "x"] },
+      open: true,
+    };
+    const actual = SettingTextualization({ layers: layers, prefixKeys: null, installed_macros: { "ProconBypassMan::Splatoon2::Macro::FastReturn": true } });
+    yaml.load(actual);
+    const expected = `version: 1.0
+setting: |-
+  install_macro_plugin ProconBypassMan::Splatoon2::Macro::FastReturn
+
+  prefix_keys_for_changing_layer %i()
+  layer :up do
+    macro ProconBypassMan::Splatoon2::Macro::FastReturn, if_pressed: %i(y l)
+  end
+  layer :right do
+
+  end
+  layer :down do
+    flip :y, if_pressed: %i(a b), force_neutral: %i(y x)
+
+  end
+  layer :left do
+
+  end`;
+    expect(actual).toBe(expected);
+  })
 })
--- src/lib/setting_textualization.ts
+++ src/lib/setting_textualization.ts
@@ -1,5 +1,6 @@
-import { Button } from "../types/button";
-import { InstalledPlugin, LayersSetting } from "../types/setting";
+import { Button, buttons } from "../types/button";
+import { LayerKey } from "../types/layer_key";
+import { InstalledPlugin, LayersSetting, Layer, MacroTable } from "../types/setting";
 
 type Props = {
   layers: LayersSetting;
@@ -10,25 +11,116 @@ type Props = {
 export const SettingTextualization = ({ layers, prefixKeys, installed_macros }: Props) => {
   const pk = prefixKeys || [];
   const normalizedInstalledMacros = installed_macros || {};
+  if (!layers.up.macro) {
+    layers.up.macro = {};
+  }
+  if (!layers.down.macro) {
+    layers.down.macro = {};
+  }
+  if (!layers.right.macro) {
+    layers.right.macro = {};
+  }
+  if (!layers.left.macro) {
+    layers.left.macro = {};
+  }
 
+  type defineButtonMethodProps = {
+    layer: Layer;
+    button: Button;
+  };
+
+  const layerBlock = (layerKey: LayerKey) => {
+    return `layer :${layerKey} do`;
+  };
+  const createButtonMethod = ({ layer, button }: defineButtonMethodProps) => {
+    const flip = layer[button].flip;
+    const remap = layer[button].remap;
+    if (flip && flip.enable) {
+      if (!flip.if_pressed) {
+        return;
+      }
+      if (flip.if_pressed.length === 0) {
+        return `flip :${button}${
+          (flip.force_neutral || "") &&
+          `, force_neutral: %i(${flip.force_neutral?.join(" ")})`
+        }`;
+      } else {
+        // ex) flip :a
+        //     flip :a, if_pressed: [:b]
+        return `flip :${button}${
+          (flip.if_pressed || "") &&
+          `, if_pressed: %i(${flip.if_pressed?.join(" ")})`
+        }${
+          (flip.force_neutral || "") &&
+          `, force_neutral: %i(${flip.force_neutral?.join(" ")})`
+        }`;
+      }
+    } else {
+      // flipとremapは共存できないのでelseにする
+      if (remap) {
+        return `remap :${button}${
+          (remap.to || "") && `, to: %i(${remap.to.join(" ")})`
+        }`;
+      }
+    }
+    return null;
+  };
+
+  const expandMacroInLayer = (macro: MacroTable) => {
+    return Object.entries(macro).map((m) => {
+      const name = m[0] as string;
+      const ifPressed = m[1] as Array<Button>;
+      return `${layerBlockIndent}macro ${name}, if_pressed: %i(${ifPressed.join(
+        " "
+      )})`;
+    });
+  };
+
+
+  const layerBlockIndent = "    ";
   const topLevelIndent = "  ";
+
 
   const result = `version: 1.0
 setting: |-
 ${Object.keys(normalizedInstalledMacros).map((name) => `${topLevelIndent}install_macro_plugin ${name}`).join("\n")}
 
   prefix_keys_for_changing_layer %i(${pk.join(" ")})
-
-  layer :up do
+  ${buttons.reduce((a, b) => {
+    const m = createButtonMethod({ layer: layers.up, button: b });
+    if (m) {
+      a = a + `\n${layerBlockIndent}` + m;
+    }
+    return a;
+  }, layerBlock("up"))}
+${expandMacroInLayer(layers.up.macro)}
   end
-
-  layer :right do
+  ${buttons.reduce((a, b) => {
+    const m = createButtonMethod({ layer: layers.right, button: b });
+    if (m) {
+      a = a + `\n${layerBlockIndent}` + m;
+    }
+    return a;
+  }, layerBlock("right"))}
+${expandMacroInLayer(layers.down.macro)}
   end
-
-  layer :down do
+  ${buttons.reduce((a, b) => {
+    const m = createButtonMethod({ layer: layers.down, button: b });
+    if (m) {
+      a = a + `\n${layerBlockIndent}` + m;
+    }
+    return a;
+  }, layerBlock("down"))}
+${expandMacroInLayer(layers.down.macro)}
   end
-
-  layer :left do
+  ${buttons.reduce((a, b) => {
+    const m = createButtonMethod({ layer: layers.left, button: b });
+    if (m) {
+      a = a + `\n${layerBlockIndent}` + m;
+    }
+    return a;
+  }, layerBlock("left"))}
+${expandMacroInLayer(layers.left.macro)}
   end`;
 
   return result.replace(/[\r\n]{3,}/g, "\n");
