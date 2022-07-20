ac7f635f5227a4ba61e24995ecec8a74b011836c
jiikko <n905i.1214@gmail.com>
2022-07-20 15:42:31 +0900

    compaction

--- src/lib/setting_textualization.test.ts
+++ src/lib/setting_textualization.test.ts
@@ -20,20 +20,21 @@ describe("prefixKeysに値があるとき", () => {
 
     const expected = `version: 1.0
 setting: |-
-
   prefix_keys_for_changing_layer %i(b)
 
   layer :up do
   end
+
   layer :right do
   end
+
   layer :down do
   end
+
   layer :left do
   end`;
     expect(actual).toBe(expected);
   });
-
 
   it("設定ファイルを出力すること", () => {
     const actual = SettingTextualization({ layers: null, prefixKeys: ["b", "y"], installed_macros: null });
@@ -41,20 +42,22 @@ setting: |-
 
     const expected = `version: 1.0
 setting: |-
-
   prefix_keys_for_changing_layer %i(b y)
 
   layer :up do
   end
+
   layer :right do
   end
+
   layer :down do
   end
+
   layer :left do
   end`;
     expect(actual).toBe(expected);
   });
+})
 
-
-
+describe("installed_macrosに値があるとき", () => {
 })
--- src/lib/setting_textualization.ts
+++ src/lib/setting_textualization.ts
@@ -9,18 +9,27 @@ type Props = {
 
 export const SettingTextualization = ({ layers, prefixKeys, installed_macros }: Props) => {
   const pk = prefixKeys || [];
+  const normalizedInstalledMacros = installed_macros || {};
 
-  return `version: 1.0
+  const topLevelIndent = "  ";
+
+  const result = `version: 1.0
 setting: |-
+${Object.keys(normalizedInstalledMacros).map((name) => `${topLevelIndent}install_macro_plugin ${name}`).join("\n")}
 
   prefix_keys_for_changing_layer %i(${pk.join(" ")})
 
   layer :up do
   end
+
   layer :right do
   end
+
   layer :down do
   end
+
   layer :left do
   end`;
+
+  return result.replace(/[\r\n]{3,}/g, "\n");
 };
