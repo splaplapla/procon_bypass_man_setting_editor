6392bf3322e529677bad2d0fd96db0c8fc9163b5
jiikko <n905i.1214@gmail.com>
2022-12-08 09:22:05 +0900

    使用可能プラグインの一覧を開閉できるようにする

---
+++ src/components/toggleable.tsx
@@ -0,0 +1,28 @@
+import { jsx, css } from "@emotion/react";
+import React, { useState, useEffect, useContext } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const Toggleable: React.FC<Props> = ({ children }) => {
+  const [available, changeVisible] = useState(false);
+  const [visibleMark, updateVisibleMark] = useState("+");
+
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    if (available) {
+      changeVisible(false);
+      updateVisibleMark("+");
+    } else {
+      changeVisible(true);
+      updateVisibleMark("-");
+    }
+  };
+
+  return (
+    <>
+      <span onClick={handleClick}>[{visibleMark}]</span>
+      {available && children}
+    </>
+  );
+};
